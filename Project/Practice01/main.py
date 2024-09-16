9209ee66e1c0a356186f9a2da2a6853ece0474e9
zerotwo <3268048793@qq.com>
2024-09-16 10:34:37 +0800

    Initial commit

--- Project/Practice01/main.py
+++ Project/Practice01/main.py
@@ -1,5 +1,8 @@
-from fileReader import FileReader,TextFileReader,JsonFileReader
+from fileReader import TextFileReader,JsonFileReader
 from data_define import Record
+from pyecharts.charts import Bar
+from pyecharts.options import *
+from pyecharts.globals import ThemeType
 
 txt_file_reader = TextFileReader("D:\\2011年1月销售数据.txt")
 json_file_reader = JsonFileReader("D:\\2011年2月销售数据JSON.txt")
@@ -18,5 +21,14 @@ for my_data in all_data:
         data_dict[my_data.date] += my_data.money
     else:
         data_dict[my_data.date] = my_data.money
-
 print(data_dict)
+
+# 数据可视化
+bar = Bar(init_opts=InitOpts(theme=ThemeType.DARK))
+
+bar.add_xaxis(list(data_dict.keys())) # 接收list 类型
+bar.add_yaxis("销售额", list(data_dict.values()),label_opts=LabelOpts(is_show=False))
+bar.set_global_opts(
+    title_opts=TitleOpts(title="每日销售额")
+)
+bar.render("每日销售额柱状图.html")
