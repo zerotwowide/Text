efda288df7afa14c80382798016f2c3c9a9bea90
zerotwo <3268048793@qq.com>
2024-09-16 09:23:00 +0800

    Initial commit

--- Project/Practice02_book/main.py
+++ Project/Practice02_book/main.py
@@ -1,17 +1,33 @@
+from itertools import count
+
 from Project.Practice02_book.Book_func import BookFunc,AchFunc
-
+from Project.Practice02_book.Book import Book
 text = AchFunc()
-dep_book = None
-for i in range(1,3):
-    print(f"录入的第{i}本书")
-    dep_book = text.deposit_book()
-# 录入后的图书信息
-for book in dep_book:
-    print(book.__str__())
+dep_book: list[Book] = []
 
-find_book = text.find_book()
-print(f"你查找的书籍信息为：{find_book}")
-text.delete_book()
-# 打印处理后的图书信息
-for book in dep_book:
-    print(book.__str__())
+# 操作页面
+print("------图书管理系统------")
+judge = input("请输入你要进行的操作：\n"
+              "1 录入书籍\n"
+              "2 查找书籍\n"
+              "3 删除书籍\n")
+print("------------------------")
+if judge == "1":
+    num:int =int(input("请输入你要录入的书籍数量："))
+    for i in range(1, num+1):
+        print(f"录入的第{i}本书")
+        dep_book = text.deposit_book()
+        # 录入后的图书信息
+        print("录入完成的图书信息为")
+        for book in dep_book:
+            print(book.__str__())
+elif judge == "2":
+    find_book = text.find_book()
+    print(f"查找的书籍信息为：{find_book}")
+elif judge == "3":
+    text.delete_book()
+    print("删除后的图书信息为：")
+    for book in dep_book:
+        print(book.__str__())
+else:
+    print("输入错误，关闭系统")
