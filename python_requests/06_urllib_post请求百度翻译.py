0c9c7631600cc56187ec4de6c619615d033783dc
zerotwo <3268048793@qq.com>
2024-10-07 15:52:25 +0800

    Initial commit

--- python_requests/06_urllib_post请求百度翻译.py
+++ python_requests/06_urllib_post请求百度翻译.py
@@ -16,12 +16,15 @@ data = {
 # post请求的参数 必须进行编码
 data = urllib.parse.urlencode(data).encode('utf-8')
 
+# 定制请求对象
 request = urllib.request.Request(url=url, data=data, headers=headers)
 
+# 发送请求
 response = urllib.request.urlopen(request)
 
+# 获取信息
 content = response.read().decode('utf-8')
 
-# print(content)
+# 转译
 obj = json.loads(content)
 print(obj)
